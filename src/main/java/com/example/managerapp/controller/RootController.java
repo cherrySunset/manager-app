eca0d639f3adcda3c9c572ac88322eb8426e820c
cherrySunset <a.kartavitsky@gmail.com>
2025-06-10 02:00:59 +0300

    Update: update RootController to redirect on swagger

--- src/main/java/com/example/managerapp/controller/RootController.java
+++ src/main/java/com/example/managerapp/controller/RootController.java
@@ -1,13 +1,13 @@
 package com.example.managerapp.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class RootController {
 
     @GetMapping("/")
-    public String home() {
-        return "Application is running!";
+    public String redirectToSwagger() {
+        return "redirect:/swagger-ui.html";
     }
 }
